224e7aae8438a1f67b9d51dd43d7b5fb7990942b
MatthewSole <91541357+MatthewSole101@users.noreply.github.com>
2024-08-02 08:47:29 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 from typing import Tuple
 import random
-
+import time
 import pygame
 
 from player import Player
@@ -29,6 +29,13 @@ player1height = 10
 player = Player(Position(250, 250))
 
 
+# Above text
+# pygame.display.set_caption()
+
+font = pygame.font.SysFont('Comic Sans', 10)
+
+start_time = time.time()
+
 while run:
 
     mousepos = pygame.mouse.get_pos()
@@ -39,12 +46,15 @@ while run:
 
     for blob in blobs:
         if player.collide(blob):
-            player.size += 1
+
+            player.size += blob.size
             blob.randomly_reposition()
             #blob.consumed = True
 
         random_colour = blob.randomcolour()
         blob.draw(window, random_colour)
+
+
 
     #blobs = [blob for blob in blobs if not blob.consumed]
 
@@ -53,6 +63,14 @@ while run:
     player.update()
     player.draw(window)
 
+
+    size_text = font.render('Size: ' + str(player.size), True, (000, 000, 000))
+    window.blit(size_text, (0, 0))
+
+    time_text = font.render('Time: ' + str(round(time.time() - start_time, 1)), True, (000, 000, 000))
+    window.blit(time_text, (250, 0))
+
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             #quits game
@@ -60,13 +78,12 @@ while run:
 
     print(mousepos)
 
-    player.update()
-    player.draw(window)
-
     key = pygame.key.get_pressed()
 
     pygame.display.update()
     clock.tick(FPS)
+end_time = time.time()
+print(round(end_time - start_time, 1), ' seconds')
 
 pygame.quit()
 
